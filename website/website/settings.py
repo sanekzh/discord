ffb7f12a70cbd36426006698de4869616207328b
FZX <FZX@users.noreply.github.com>
2019-01-11 23:30:05 +0400

    Static file configuration

--- website/website/settings.py
+++ website/website/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = '/home/ubuntu/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
+
 
 
 # Paypal
