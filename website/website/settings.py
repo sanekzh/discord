20447a31336e79799f7f1ef10d76e701a279f92e
sanek <aksm.sanek@gmail.com>
2019-02-17 00:12:01 +0200

    add host

--- website/website/settings.py
+++ website/website/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
         'CONN_MAX_AGE': 3000, 
         # 'NAME': 'db_django',
         'NAME': 'discord_db',
