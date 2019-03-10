85d5a42dd2c8b1ebe3fecc2c16180f1fba6dbf08
sanek <aksm.sanek@gmail.com>
2019-03-11 01:03:48 +0200

    reworking template

--- website/announceusio/views.py
+++ website/announceusio/views.py
@@ -123,7 +123,8 @@ class DashboardView(View):
                                                    is_activated=True).count()
             members_all = Member.objects.filter(user=User.objects.get(username=request.user)).count()
             owner_members_email_list = Member.objects.filter(user=User.objects.get(username=request.user)).values_list('email', flat=True)
-            members = Member.objects.filter(user=User.objects.get(username=request.user)).order_by('-id')[:10]
+            members = Member.objects.filter(user=User.objects.get(username=request.user),
+                                            discord_username__isnull=False).order_by('-id')[:10]
             user = User.objects.get(username=request.user)
             billing = Billing.objects.get(user=user)
             income = PayPalIPN.objects.filter(business=billing.paypal_email, created_at__gte=timezone.now(). \
