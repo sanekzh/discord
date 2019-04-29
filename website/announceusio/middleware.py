a06a6d7f10bc73dc79f1aeb77fa1aba200a4d921
sanek <aksm.sanek@gmail.com>
2019-04-30 00:22:35 +0300

    reworking template

--- website/announceusio/middleware.py
+++ website/announceusio/middleware.py
@@ -20,7 +20,7 @@ class GetUsername(MiddlewareMixin):
     def process_request(self, request):
         try:
             user_profile = UserProfile.objects.filter(user=User.objects.get(username=request.user)).first()
-            request.user_name = user_profile.company.capitalize() if user_profile.company else ''
+            request.user_name = user_profile.company if user_profile.company else ''
             return None
         except Exception as e:
             return None
